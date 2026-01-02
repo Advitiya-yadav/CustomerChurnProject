725adcc44e931af3f368ec1cbcf76c4528a53505
Advitiya Yadav <advitiyayadav2105@gmail.com>
2026-01-02 11:40:56 +0530

    Streamlit App

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,11 @@
-# NOW WE WILL CREATE THE FRONTEND OF A WEBAPP SO THAT USERS CAN GET CHURN VALUES FOR THEIR DATA
-
 import streamlit as st
 import joblib
 import numpy as np
 
+st.set_page_config(page_title="Churn Prediction", layout="centered")
+
 scaler = joblib.load("scaler.pkl")
 model = joblib.load("model.pkl")
-
-st.set_page_config(page_title="Churn Prediction", layout="centered")
 
 st.title("📉 Customer Churn Prediction App")
 st.caption("Predict whether a customer is likely to churn based on their details")
@@ -18,26 +16,25 @@ st.subheader("🧾 Enter Customer Details")
 col1, col2 = st.columns(2)
 
 with col1:
-    age = st.number_input("Age", min_value=5, max_value=110, value=10)
-    tenure = st.number_input("Tenure (months)", min_value=0, max_value=240, value=0)
+    age = st.number_input("Age", min_value=5, max_value=110, value=30)
+    tenure = st.number_input("Tenure (months)", min_value=0, max_value=240, value=12)
 
 with col2:
-    monthlycharge = st.number_input("Monthly Charges", min_value=25, max_value=100)
+    monthlycharge = st.number_input("Monthly Charges", min_value=25, max_value=100, value=50)
     gender = st.selectbox("Gender", ['Male', 'Female'])
 
 st.divider()
 
 predictbutton = st.button("🔍 Predict Churn")
-
 st.divider()
 
 if predictbutton:
     gender_selected = 1 if gender == "Female" else 0
 
-    X = [age, gender_selected, tenure, monthlycharge]
-    X1 = np.array(X)
+    # ORDER MUST MATCH TRAINING
+    X = [age, tenure, monthlycharge, gender_selected]
 
-    X_array = scaler.transform([X1])
+    X_array = scaler.transform([X])
     prediction = model.predict(X_array)[0]
 
     predicted = "Yes" if prediction == 1 else "No"
