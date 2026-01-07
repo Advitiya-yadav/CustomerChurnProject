52a21f43a47381e5b7c0f742ec97a3b18e32eae1
Advitiya Yadav <advitiyayadav2105@gmail.com>
2026-01-07 21:39:26 +0530

    Updated strealit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,18 +17,15 @@ st.divider()
 
 st.subheader("🧾 Enter Customer Details")
 
-col1, col2 = st.columns(2)
+age = st.number_input("Age", min_value=10, max_value=100, value=30)
+tenure = st.number_input("Tenure (months)", min_value=0, max_value=240, value=12)
+monthly_charges = st.number_input("Monthly Charges", min_value=0.0, value=70.0)
 
-with col1:
-    age = st.number_input("Age", min_value=10, max_value=100, value=30)
-    tenure = st.number_input("Tenure (months)", min_value=0, max_value=240, value=12)
-    monthly_charges = st.number_input("Monthly Charges", min_value=0.0, value=70.0)
 
-with col2:
-    gender = st.selectbox("Gender", ["Male", "Female"])
-    contract = st.selectbox("Contract Type",["Month-to-month", "One year", "Two year"])
-    internet = st.selectbox("Internet Service",["DSL", "Fiber optic", "No"])
-    tech_support = st.selectbox("Tech Support", ["Yes", "No"])
+gender = st.selectbox("Gender", ["Male", "Female"])
+contract = st.selectbox("Contract Type",["Month-to-month", "One year", "Two year"])
+internet = st.selectbox("Internet Service",["DSL", "Fiber optic", "No"])
+tech_support = st.selectbox("Tech Support", ["Yes", "No"])
 
 st.divider()
 
